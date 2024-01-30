822f0878841824143f1bb001dd318fbabe32cb4e
ram <ramkumarreddy92@gmail.com>
2024-01-30 10:20:05 +0530

    initial Commit

---
+++ kishore/src/test/java/com/example/kishore/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kishore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
